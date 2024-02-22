64ba5c5ffbf9018f0a2e3569b8c93d92705ea13b
Muhammadizhan <izhanayaz4@gmail.com>
2024-02-23 00:15:55 +0500

    visitor

---
+++ app.js
@@ -0,0 +1,26 @@
+var visitorName = prompt("Please enter your name:");
+
+var proudectTitle = prompt("Please enter the product title:");
+
+var quentity = prompt("Please enter the quantity of products you want to order:");
+
+var message = visitorName + "odered" + quantity + " " + productTitle + "(s) on XYZ Clothing store";
+
+document.write("<p>" + message + "</p>");
+
+
+ 
+//  var visitorName = prompt("Please enter your name:");
+
+
+//  var productTitle = prompt("Please enter the product title:");
+
+ 
+//  var quantity = prompt("Please enter the quantity of products you want to order:");
+
+
+//  var message = visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store";
+
+ 
+//  document.write("<p>" + message + "</p>");
+
